d98a512ddcfc3ef9a76ca9c3b9d1a5b36059de64
Dmitry <dimakorobitcyn@yandex.ru>
2022-09-28 23:26:31 +0500

    2.1. Добавит React-компоненты

--- project/src/index.tsx
+++ project/src/index.tsx
@@ -2,12 +2,22 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './components/app/app';
 
+const Setting = {
+  year: 2022,
+  genre: 'Horror',
+  title: 'Life'
+};
+
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement,
 );
 
 root.render(
   <React.StrictMode>
-    <App />
+    <App
+      year = {Setting.year}
+      genre = {Setting.genre}
+      title = {Setting.title}
+    />
   </React.StrictMode>,
 );
