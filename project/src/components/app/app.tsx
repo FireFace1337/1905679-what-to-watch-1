5ec79f40343d771017e506f478fbe9c0480a8cc9
Dmitry <dimakorobitcyn@yandex.ru>
2022-10-05 19:45:41 +0500

    3.1. Добавит маршрутизацию

--- project/src/components/app/app.tsx
+++ project/src/components/app/app.tsx
@@ -1,4 +1,13 @@
+import {Route, BrowserRouter, Routes} from 'react-router-dom';
+import {AppRoute} from '../../const';
 import MainPage from '../../pages/main-page/main-page';
+import AddReview from '../../pages/add-review-page/add-review-page';
+import Film from '../../pages/film-page/film-page';
+import MyList from '../../pages/my-list-page/my-list-page';
+import Player from '../../pages/player-page/player-page';
+import SignIn from '../../pages/sign-in-page/sign-in-page';
+import NotFoundPage from '../../pages/not-found-page/not-found-page';
+
 
 type mainInfo = {
   year: number;
@@ -8,7 +17,38 @@ type mainInfo = {
 
 function App({year, genre, title}: mainInfo): JSX.Element {
   return (
-    <MainPage mainInfo = {{year, genre, title}}/>
+    <BrowserRouter>
+      <Routes>
+        <Route
+          path={AppRoute.Main}
+          element={<MainPage mainInfo = {{year, genre, title}} />}
+        />
+        <Route
+          path={AppRoute.AddReview}
+          element={<AddReview />}
+        />
+        <Route
+          path={AppRoute.Film}
+          element={<Film />}
+        />
+        <Route
+          path={AppRoute.MyList}
+          element={<MyList />}
+        />
+        <Route
+          path={AppRoute.Player}
+          element={<Player />}
+        />
+        <Route
+          path={AppRoute.SignIn}
+          element={<SignIn />}
+        />
+        <Route
+          path="*"
+          element={<NotFoundPage />}
+        />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
---
+++ project/src/pages/not-found-page/not-found-page.tsx
@@ -0,0 +1,10 @@
+function NotFoundPage(): JSX.Element {
+  return (
+    <div>
+      <h1>404. Page not found</h1>
+      <a href="/">Вернуться на главную</a>
+    </div>
+  );
+}
+
+export default NotFoundPage;
