bc4128369ff87d3e4f7dd79f7298d3e193e95391
Dmitry <dimakorobitcyn@yandex.ru>
2022-10-16 13:48:31 +0500

    4.1 Добавит моки

---
+++ project/src/mocks/film.ts
@@ -0,0 +1,45 @@
+import {DescriptionFilm} from '../types/film';
+
+export const descriptionFilm: DescriptionFilm[] = [
+  {
+    id: 1,
+    name: 'Film1',
+    genre: 'comedy',
+    year: 2017,
+  }, {
+    id: 2,
+    name: 'Film2',
+    genre: 'horror',
+    year: 2019,
+  }, {
+    id: 3,
+    name: 'Film3',
+    genre: 'comedy',
+    year: 2018,
+  }, {
+    id: 4,
+    name: 'Film4',
+    genre: 'thriller',
+    year: 2020,
+  }, {
+    id: 5,
+    name: 'Film5',
+    genre: 'melodrama',
+    year: 2018,
+  }, {
+    id: 6,
+    name: 'Film6',
+    genre: 'drama',
+    year: 2021,
+  }, {
+    id: 7,
+    name: 'Film7',
+    genre: 'action movie',
+    year: 2022,
+  }, {
+    id: 8,
+    name: 'Film8',
+    genre: 'post-apocalyptic',
+    year: 2023,
+  },
+];
---
+++ project/src/types/film.ts
@@ -0,0 +1,6 @@
+export type DescriptionFilm = {
+    id: number;
+    name: string;
+    genre: string;
+    year: number;
+};
