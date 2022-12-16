ffedf2f746dbb04b687d7d85f7513f2e12e08f16
FireFace1337 <dimakorobitcyn@yandex.ru>
2022-12-16 23:59:13 +0500

    Complete task6.1

--- project/src/components/app/app.tsx
+++ project/src/components/app/app.tsx
@@ -1,5 +1,6 @@
-import MainContent from '../../pages/mainContent/mainContent';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { useAppSelector } from '../../hooks';
+import MainContent from '../../pages/mainContent/mainContent';
 import NotFound from '../notFound/notFound';
 import SignIn from '../../pages/signIn/signIn';
 import MyList from '../../pages/myList/myList';
@@ -7,19 +8,11 @@ import MoviePage from '../../pages/moviePage/moviePage';
 import AddReview from '../../pages/addReview/addReview';
 import Player from '../../pages/player/player';
 import PrivateRoute from '../privateRoute/privateRoute';
-import { Film } from '../../types/films';
-import { Promo } from '../../types/promo';
 import { AppRoute, AuthorizationStatus } from '../../const';
 import ScrollToTop from '../scrollToTop/scrollToTop';
 
-type AppProps = {
-  films: Film[];
-  promoFilm: Promo;
-  favouriteFilms: Film[];
-}
-
-function App(props: AppProps): JSX.Element {
-  const {films, promoFilm, favouriteFilms} = props;
+function App(): JSX.Element {
+  const {listOfFilms, promoFilm, favouriteFilms} = useAppSelector((state) => state);
 
   return (
     <BrowserRouter>
@@ -28,7 +21,7 @@ function App(props: AppProps): JSX.Element {
         <Route path='/'>
           <Route index element={
             <MainContent
-              films={films}
+              films={listOfFilms}
               promoFilm={promoFilm}
             />
           }
@@ -41,8 +34,8 @@ function App(props: AppProps): JSX.Element {
           }
           />
           <Route path={AppRoute.Films}>
-            <Route path=':filmId' element={ <MoviePage films={films} /> } />
-            <Route path={`:filmId${AppRoute.Review}`} element={ <AddReview films={films} /> } />
+            <Route path=':filmId' element={ <MoviePage films={listOfFilms} /> } />
+            <Route path={`:filmId${AppRoute.Review}`} element={ <AddReview films={listOfFilms} /> } />
           </Route>
           <Route path={AppRoute.Player}>
             <Route path=':filmId' element={ <Player /> } />
--- project/src/const.ts
+++ project/src/const.ts
@@ -17,3 +17,31 @@ export enum Tab {
   Details = 'Details',
   Reviews = 'Reviews'
 }
+
+export enum Genre {
+  AllGenres = 'AllGenres',
+  Adventure = 'Adventure',
+  Comedy = 'Comedy',
+  Crime = 'Crime',
+  Documentary = 'Documentary',
+  Drama = 'Drama',
+  Horror = 'Horror',
+  KidsAndFamily = 'KidsAndFamily',
+  Romance = 'Romance',
+  SciFi = 'SciFi',
+  Thriller = 'Thriller'
+}
+
+export const GenresObj = {
+  'All genres': Genre.AllGenres,
+  'Adventures': Genre.Adventure,
+  'Comedies': Genre.Comedy,
+  'Crime': Genre.Crime,
+  'Documentary': Genre.Documentary,
+  'Dramas': Genre.Drama,
+  'Horror': Genre.Horror,
+  'Kids & Family': Genre.KidsAndFamily,
+  'Romance': Genre.Romance,
+  'Sci-Fi': Genre.SciFi,
+  'Thrillers': Genre.Thriller
+};
